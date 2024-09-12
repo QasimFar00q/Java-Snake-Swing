51394df6fdc9efc1b1bb2d7c191fe6e56e8a2d55
Qasim Farooq <qfarooq100@gmail.com>
2024-09-12 22:09:37 +0100

    Essentials of the game complete. -Collision added. -Body trail. -Fruit. -Fruit colliding with player. -Fruit randomly generating into a non taken spot; -Game ends when colliding with body or wall. -Body length increases on fruit consumption. -Game ends at 624

--- src/main/java/org/example/GUIs/SnakeGUI.java
+++ src/main/java/org/example/GUIs/SnakeGUI.java
@@ -6,7 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.EventListener;
+import java.util.Random;
 
 import org.example.GUIs.Tile;
     public class SnakeGUI extends JPanel implements KeyListener,ActionListener {
@@ -15,12 +17,19 @@ import org.example.GUIs.Tile;
         int tileSize = 25;
 
         Timer loop;
-        //entities
+        int score;
+        //Fruit
+        Tile fruit;
+        //Player
         Tile snakeHead = new Tile(0, 0);
         Color tempCol = Color.GREEN;
-
+        boolean dead= false;
+        boolean win = false;
         int velX =0;
         int velY =1;
+        ArrayList<Tile> snakeBody = new ArrayList<Tile>();
+        Random r;
+
         public SnakeGUI (int w, int h){
             this.boardHeight = h;
             this.boardWidth = w;
@@ -28,12 +37,13 @@ import org.example.GUIs.Tile;
             setBackground(Color.BLACK);
             addKeyListener(this);
             setFocusable(true);
-
+            setVisible(true);
             loop = new Timer(100,this);
-
-            System.out.println("Timer made");
+            r = new Random();
+            fruit = new Tile(r.nextInt(25)*tileSize,r.nextInt(25)*tileSize);
+            score = 0;
             loop.start();
-            System.out.println("Timer Ran");
+
         }
 
         @Override
@@ -51,41 +61,138 @@ import org.example.GUIs.Tile;
             //player
             g.setColor(tempCol); //PLEASE CHANGE
             g.fillRect(snakeHead.x, snakeHead.y, tileSize, tileSize);
+
+            for (int i = 0; i < snakeBody.size(); i++) {
+
+                g.fillRect(snakeBody.get(i).x, snakeBody.get(i).y, tileSize, tileSize);
+
+
+            }
+
+
+            //Fruit
+            g.setColor(Color.RED);
+            g.fillRect(fruit.x, fruit.y, tileSize,tileSize);
+
+            //Score
+
+            if(dead){
+                g.setColor(Color.RED);
+                g.drawString("GAME OVER: " + score,25,25);
+            } else if (win) {
+                g.setColor(Color.GREEN);
+                g.drawString("GAME WON: " + score,25,25);
+            }else{
+                g.setColor(Color.WHITE);
+                g.drawString("Score: " + score,25,25);
+            }
         }
 
         public void move(){
-            snakeHead.x +=velX*tileSize;
-            snakeHead.y +=velY*tileSize;
-        }
+            //Load next co-ordinates in order to verify if suitable to move onto
+            int newXPos = snakeHead.x;
+            int newYpos = snakeHead.y;
+            newXPos += velX * tileSize;
+            newYpos += velY * tileSize;
 
-        @Override
-        public void keyPressed(KeyEvent e) {
-            System.out.println("keyPressed");
-            System.out.println("Key pressed: " + e.getKeyChar());
 
-            if(e.getKeyCode() == KeyEvent.VK_UP){
-                velY =-1;
-                velX = 0;
-            }else if(e.getKeyCode() == KeyEvent.VK_DOWN){
-                velY = 1;
-                velX = 0;
-            }else if(e.getKeyCode() == KeyEvent.VK_LEFT){
-                velY = 0;
-                velX = -1;
-            }else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-                velY = 0;
-                velX = 1;
+
+            if (!gameOver(newXPos,newYpos)) {
+                if(!dead) {
+                    int oldXpos = snakeHead.x;
+                    int oldYpos = snakeHead.y;
+                    //If not dead and next tile is valid to move, proceed with move
+                    snakeHead.x = newXPos;
+                    snakeHead.y = newYpos;
+                    if(snakeBody.size() > 0) {
+                        for (int i = 0; i < snakeBody.size(); i++) {
+
+                            int currentBodyPosX = snakeBody.get(i).x;
+                            int currentBodyPosY = snakeBody.get(i).y;
+                            snakeBody.get(i).x = oldXpos;
+                            snakeBody.get(i).y = oldYpos;
+                            oldXpos = currentBodyPosX;
+                            oldYpos = currentBodyPosY;
+                        }
+                    }
+
+                    if(snakeHead.x == fruit.x && snakeHead.y == fruit.y ){
+                        snakeBody.add(new Tile(999,999));
+                        boolean fruitValid = false;
+                        while (!fruitValid) {
+                            fruitValid = true;
+                            fruit = new Tile(r.nextInt(25)*tileSize,r.nextInt(25)*tileSize);
+                            System.out.println("Random generated");
+                            for (int i = 0; i < snakeBody.size(); i++) {
+
+                               if(fruit.x == snakeBody.get(i).x && fruit.y == snakeBody.get(i).y){
+                                   fruitValid = false;
+                                   System.out.println("Body Collision");
+                               }
+                            }
+                        }
+                        fruit = new Tile(r.nextInt(25)*tileSize,r.nextInt(25)*tileSize);
+                        score += 1;
+                        System.out.println(score);
+                    }
+                }
+            }else{
+                dead = true;
             }
         }
 
         @Override
-        public void actionPerformed(ActionEvent e) {
+        public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_UP && velY != 1){
+                    velY =-1;
+                    velX = 0;
+                }else if(e.getKeyCode() == KeyEvent.VK_DOWN && velY != -1 ){
+                    velY = 1;
+                    velX = 0;
+                }else if(e.getKeyCode() == KeyEvent.VK_LEFT && velX != 1){
+                    velY = 0;
+                    velX = -1;
+                }else if(e.getKeyCode() == KeyEvent.VK_RIGHT && velX != -1){
+                    velY = 0;
+                    velX = 1;
+                }
 
-
-            move();
-            repaint();
         }
 
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            move();
+            repaint();
+
+            if (win){
+                loop.stop();
+            } else if (dead) {
+                loop.stop();
+            }
+        }
+
+        public boolean gameOver(int newX, int newY){
+
+
+            if(score >= 624){
+                win = true;
+                return false;
+            }
+            if (newX >= 625 || newX < 0 || newY >= 625 || newY < 0 ){
+                return true ;
+            }else{
+                boolean validMove = true;
+                for (int i = 0; i < snakeBody.size(); i++) {
+                    if (newX == snakeBody.get(i).x && newY == snakeBody.get(i).y) {
+                        validMove = false;
+                    }
+                }
+                if (validMove){
+                    return false;
+                }
+                return  true;
+            }
+        }
 
         //Unncessary methods
         @Override
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         JFrame frame = new JFrame("Game");
         frame.setVisible(true);
-        frame.setSize(610,610);
+        frame.setSize(625,625);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,8 +23,9 @@ public class Main {
         SnakeGUI game = new SnakeGUI(625,625);
         frame.add(game);
         frame.pack();
-        frame.requestFocus();
 
+        frame.requestFocus();
+                                                        
     }
 }
 
